e1f3f5a62916ecfe6184cac88414c7f27817fa84
Randy Johnson <rj9317@gmail.com>
2025-06-03 13:28:21 -0600

    feat: implement 'wireguard get wg-conf'

--- cmd/wireguard/cmd/wgconf.go
+++ cmd/wireguard/cmd/wgconf.go
@@ -1,16 +1,29 @@
 package cmd
 
 import (
+	"context"
+	"encoding/base64"
 	"errors"
+	"fmt"
+	"net"
 	"net/http"
 	"os"
 
+	"github.com/randalljohnson/wireguard-threeport-module/internal/wireguard"
+	wg_client "github.com/randalljohnson/wireguard-threeport-module/pkg/client/v0"
 	cobra "github.com/spf13/cobra"
 	tptctl_cmd "github.com/threeport/threeport/cmd/tptctl/cmd"
 	cli "github.com/threeport/threeport/pkg/cli/v0"
+	tptctl_config "github.com/threeport/threeport/pkg/config/v0"
+	kube "github.com/threeport/threeport/pkg/kube/v0"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 )
 
 var getWireguardConfVersion string
+var wireguardInstanceName string
 
 // GetWireguardConfCmd represents the wireguard-definition command
 var GetWireguardConfCmd = &cobra.Command{
@@ -18,10 +31,32 @@ var GetWireguardConfCmd = &cobra.Command{
 	Long:    "Get wireguard configuration from the system.",
 	PreRun:  CommandPreRunFunc,
 	Run: func(cmd *cobra.Command, args []string) {
+
 		apiClient, _, apiEndpoint, requestedControlPlane := tptctl_cmd.GetClientContext(cmd)
 
-		switch getWireguardDefinitionVersion {
+		// get encryption key from threeport config
+		threeportConfig, requestedControlPlane, err := tptctl_config.GetThreeportConfig(cliArgs.ControlPlaneName)
+		if err != nil {
+			cli.Error("failed to get threeport config: %w", err)
+			os.Exit(1)
+		}
+
+		// get encryption key
+		encryptionKey, err := threeportConfig.GetThreeportEncryptionKey(requestedControlPlane)
+		if err != nil {
+			cli.Error("failed to get encryption key from threeport config: %w", err)
+			os.Exit(1)
+		}
+
+		switch getWireguardConfVersion {
 		case "v0":
+			wireguardConf, err := getWireguardConfVersionV0(apiClient, apiEndpoint, encryptionKey)
+			if err != nil {
+				cli.Error("failed to retrieve wireguard configuration", err)
+				os.Exit(1)
+			}
+
+			fmt.Println(wireguardConf)
 		default:
 			cli.Error("", errors.New("unrecognized object version"))
 			os.Exit(1)
@@ -39,12 +74,205 @@ func init() {
 		&getWireguardConfVersion,
 		"version", "v", "v0", "Version of wireguard configuration object to retrieve. One of: [v0]",
 	)
+	GetWireguardConfCmd.Flags().StringVarP(
+		&wireguardInstanceName,
+		"name", "n", "", "Name of wireguard instance to retrieve configuration for.",
+	)
+	GetWireguardConfCmd.MarkFlagRequired("name")
 }
 
 // getWireguardConfVersionV0 retrieves the wireguard configuration from the system
 func getWireguardConfVersionV0(
 	apiClient *http.Client,
 	apiEndpoint string,
+	encryptionKey string,
 ) (string, error) {
-	return "", nil
+
+	// get wireguard instance
+	wireguardInstance, err := wg_client.GetWireguardInstanceByName(
+		apiClient,
+		apiEndpoint,
+		wireguardInstanceName,
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed to get wireguard instance: %w", err)
+	}
+
+	// get wireguard service
+	wireguardService, err := wireguard.GetWireguardService(
+		apiClient,
+		apiEndpoint,
+		encryptionKey,
+		wireguardInstance,
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed to get wireguard service: %w", err)
+	}
+
+	// get wireguard loadbalancer ip address
+	ingressSlice, found, err := unstructured.NestedSlice(
+		wireguardService.Object,
+		"status",
+		"loadBalancer",
+		"ingress",
+	)
+	switch {
+	case err != nil:
+		return "", fmt.Errorf("failed to get wireguard loadbalancer ip address: %w", err)
+	case !found:
+		return "", fmt.Errorf("failed to find wireguard loadbalancer ip address")
+	}
+
+	// get public-facing ip address
+	var publicFacingIpAddress string
+	for _, ingress := range ingressSlice {
+		// convert ingress to map
+		ingressMap, ok := ingress.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		// get ip address
+		ip, found, err := unstructured.NestedString(ingressMap, "ip")
+		if err != nil || !found {
+			continue
+		}
+
+		// parse and validate ip address
+		parsedIP := net.ParseIP(ip)
+		if parsedIP == nil {
+			continue
+		}
+
+		// return if not private
+		if !parsedIP.IsPrivate() {
+			publicFacingIpAddress = ip
+			break
+		}
+	}
+
+	// get wireguard private key secrets
+	// get wireguard kubernetes runtime instance
+	kubernetesRuntimeInstance, err := wireguard.GetWireguardKubernetesRuntimeInstance(
+		apiClient,
+		apiEndpoint,
+		wireguardInstance,
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed to get wireguard kubernetes runtime instance: %w", err)
+	}
+
+	// get wireguard secrets
+	kubeClient, _, err := kube.GetClient(
+		kubernetesRuntimeInstance,
+		false,
+		apiClient,
+		apiEndpoint,
+		encryptionKey,
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed to get kubernetes client: %w", err)
+	}
+
+	// get wireguard server secret
+	serverSecret, err := getWireguardSecret(
+		kubeClient,
+		"wireguard-server-keys",
+		*wireguardInstance.Name,
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed to get wireguard server secret: %w", err)
+	}
+
+	// get wireguard client secret
+	clientSecret, err := getWireguardSecret(
+		kubeClient,
+		"wireguard-client-keys",
+		*wireguardInstance.Name,
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed to get wireguard client secret: %w", err)
+	}
+
+	// get wireguard server public key
+	serverPublicKey, err := getWireguardSecretData(serverSecret, "publicKey")
+	if err != nil {
+		return "", fmt.Errorf("failed to get wireguard server private and public keys: %w", err)
+	}
+
+	// get wireguard client private key
+	clientPrivateKey, err := getWireguardSecretData(clientSecret, "privateKey")
+	if err != nil {
+		return "", fmt.Errorf("failed to get wireguard client private and public keys: %w", err)
+	}
+
+	return fmt.Sprintf(`[Interface]
+PrivateKey = %s
+Address = 10.0.0.2/24
+DNS = 8.8.8.8
+
+[Peer]
+PublicKey = %s
+AllowedIPs = 0.0.0.0/0
+Endpoint = %s:51820
+PersistentKeepalive = 25`,
+		clientPrivateKey,
+		serverPublicKey,
+		publicFacingIpAddress,
+	), nil
+}
+
+// getWireguardSecret returns a secret with a given name
+func getWireguardSecret(
+	kubeClient dynamic.Interface,
+	secretName string,
+	secretNamespace string,
+) (unstructured.Unstructured, error) {
+	// list wireguard secrets
+	secretsList, err := kubeClient.Resource(
+		schema.GroupVersionResource{
+			Group:    "",
+			Version:  "v1",
+			Resource: "secrets",
+		},
+	).Namespace(secretNamespace).List(
+		context.Background(),
+		metav1.ListOptions{
+			FieldSelector: fmt.Sprintf("metadata.name=%s", secretName),
+		},
+	)
+	switch {
+	case err != nil:
+		return unstructured.Unstructured{}, fmt.Errorf("failed to list secrets: %w", err)
+	case len(secretsList.Items) == 0:
+		return unstructured.Unstructured{}, fmt.Errorf("failed to find wireguard secrets")
+	case len(secretsList.Items) > 1:
+		return unstructured.Unstructured{}, fmt.Errorf("found multiple wireguard secrets")
+	}
+
+	// return secret
+	return secretsList.Items[0], nil
+}
+
+// getWireguardSecretData returns the private and public keys from a wireguard secret
+func getWireguardSecretData(
+	secret unstructured.Unstructured,
+	key string,
+) (string, error) {
+	data, found, err := unstructured.NestedMap(secret.Object, "data")
+	if err != nil || !found {
+		return "", fmt.Errorf("failed to get secret data: %w", err)
+	}
+
+	valueBase64, ok := data[key].(string)
+	if !ok {
+		return "", fmt.Errorf("privateKey not found in secret")
+	}
+
+	value, err := base64.StdEncoding.DecodeString(valueBase64)
+	if err != nil {
+		return "", fmt.Errorf("failed to decode privateKey: %w", err)
+	}
+
+	return string(value), nil
 }
--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/spf13/cobra v1.9.1
 	github.com/threeport/threeport v0.6.1
 	go.uber.org/zap v1.27.0
+	golang.org/x/crypto v0.38.0
 	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.1
 	gorm.io/driver/postgres v1.5.11
@@ -208,7 +209,6 @@ require (
 	go.opentelemetry.io/otel/trace v1.35.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/exp v0.0.0-20250506013437-ce4c2cf36ca6 // indirect
 	golang.org/x/net v0.40.0 // indirect
 	golang.org/x/oauth2 v0.27.0 // indirect
